848b0e3e5037dc17b70f994b6aa6b8e401b9ea1c
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2020-01-24 07:44:44 -0500

    Update kali-ganglia-deploy.sh

--- kali-ganglia-deploy.sh
+++ kali-ganglia-deploy.sh
@@ -1,7 +1,7 @@
 export XDG_RUNTIME_DIR=/run/user/%I && /usr/lib/systemd/systemd --user &
 
 
-docker run -it -d --network="host" --restart="always" --privileged=true \
+docker run -it -d  --network="host" --restart="always" --privileged=true \
 --name "kali-ganglia-mainunit" \
 --device /dev/snd \
 -e DISPLAY=$DISPLAY \
@@ -9,7 +9,6 @@ docker run -it -d --network="host" --restart="always" --privileged=true \
 -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
 --entrypoint "/sbin/init" \
-
 c4pt/kali-ganglia-main-monitor-unit:web-main-unit
 
 docker exec -it kali-ganglia-mainunit /bin/bash
