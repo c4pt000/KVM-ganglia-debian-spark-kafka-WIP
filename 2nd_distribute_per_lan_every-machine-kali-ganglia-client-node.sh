bf787a378d3a4d4ffa5675363685ba5b5c41b926
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2020-01-26 07:53:21 -0500

    Update 2nd_distribute_per_lan_every-machine-kali-ganglia-client-node.sh

--- 2nd_distribute_per_lan_every-machine-kali-ganglia-client-node.sh
+++ 2nd_distribute_per_lan_every-machine-kali-ganglia-client-node.sh
@@ -3,11 +3,6 @@ export XDG_RUNTIME_DIR=/run/user/%I && /usr/lib/systemd/systemd --user &
 
 docker run -it -d  --network="host" --restart="always" --privileged=true \
 --name "kali-ganglia-a-node-client" \
---device /dev/snd \
--e DISPLAY=$DISPLAY \
---stop-signal=SIGRTMIN+3 --cap-add=SYS_ADMIN --security-opt=seccomp:unconfined \
--v /sys/fs/cgroup:/sys/fs/cgroup:ro \
--v /tmp/.X11-unix:/tmp/.X11-unix \
 --entrypoint "/sbin/init" \
 c4pt/kali-ganglia-client-node:a_client_node
 
