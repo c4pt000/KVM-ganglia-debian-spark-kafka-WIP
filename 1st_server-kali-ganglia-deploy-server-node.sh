63ac9c8248a059e1a945d25ccd93e903ca2104eb
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2020-01-26 07:28:42 -0500

    Update 1st_server-kali-ganglia-deploy-server-node.sh

--- 1st_server-kali-ganglia-deploy-server-node.sh
+++ 1st_server-kali-ganglia-deploy-server-node.sh
@@ -1,16 +1,13 @@
+
 export XDG_RUNTIME_DIR=/run/user/%I && /usr/lib/systemd/systemd --user &
 
 
 docker run -it -d  --network="host" --restart="always" --privileged=true \
---name "kali-ganglia-mainunit" \
---device /dev/snd \
--e DISPLAY=$DISPLAY \
---stop-signal=SIGRTMIN+3 --cap-add=SYS_ADMIN --security-opt=seccomp:unconfined \
+--name "kali-ganglia-cluster-main" \
 -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
 --entrypoint "/sbin/init" \
-c4pt/kali-ganglia-main-monitor-unit:web-main-unit
-
-docker exec -it kali-ganglia-mainunit /bin/bash &
+c4pt/kali-gangila-cluster-main
+docker exec -it kali-ganglia-cluster-main /bin/bash &
 
 firefox http://127.0.0.1/ganglia
